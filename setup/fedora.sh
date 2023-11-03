da4be082564e0b901c03a459b30e46eb0d626b6b
anmol ranjan <88226614+Nakamerica@users.noreply.github.com>
2023-11-03 07:13:35 +0530

    Update fedora.sh
    Use rawgit to fetch the 51-android.rules file instead of fetching it directly from github to avoid the 'Recv failure: Connection reset by peer' error.

--- setup/fedora.sh
+++ setup/fedora.sh
@@ -63,7 +63,7 @@ sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleap
 sudo chmod a+rx /usr/local/bin/repo
 
 echo -e "Setting up udev rules for ADB!"
-sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://cdn.rawgit.com/M0Rf30/android-udev-rules/master/51-android.rules
 sudo chmod 644 /etc/udev/rules.d/51-android.rules
 sudo chown root /etc/udev/rules.d/51-android.rules
 sudo udevadm control --reload-rules
